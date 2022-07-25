49cebbef1eec103800c8342177b68ecc6c05535f
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-07-25 01:57:50 -0300

    Chore: Created the games routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import categoriesRoutes from "./routes/categoriesRoutes.js";
+import { gamesRoutes } from "./routes/gamesRoutes.js";
 dotenv.config();
 
 const server = express();
@@ -9,6 +10,7 @@ const server = express();
 server.use(cors(), json());
 
 server.use(categoriesRoutes);
+server.use(gamesRoutes);
 
 server.listen(process.env.PORT, () => {
   console.log(`The server is runnig on port: ${process.env.PORT}`);
