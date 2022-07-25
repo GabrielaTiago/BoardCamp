418245ed7e21446ea0fed1ac9de691ed35811149
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-07-24 23:39:41 -0300

    Chore: Created the categories routes

---
+++ routes/categoriesRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const categoriesRoutes = Router();
+
+categoriesRoutes.get("/categories");
+categoriesRoutes.post("/categories");
+
+export default categoriesRoutes;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import categoriesRoutes from "./routes/categoriesRoutes.js";
 dotenv.config();
 
 const server = express();
 
 server.use(cors(), json());
+
+server.use(categoriesRoutes);
 
 server.listen(process.env.PORT, () => {
   console.log(`The server is runnig on port: ${process.env.PORT}`);
