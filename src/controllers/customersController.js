8029e3550ec8cbc7ad0ce9a0f6cf5ffe3e381a87
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-07-26 11:18:09 -0300

    Feat: Creted the getCustomers controller

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,24 +1,34 @@
 import { connection } from "../databases/postgres.js";
 
 async function getCustomers(req, res) {
-  const cpf = req.query;
+  const { cpf } = req.query;
 
   try {
+    const params = [];
+    let whereClause = "";
+
+    if (cpf) {
+      params.push(`%${cpf}%`);
+      whereClause += `WHERE cpf ILIKE $${params.length}`;
+    }
+
     const { rows: allCustomers } = await connection.query(
-      `SELECT *
-      FROM customers
-      WHERE customers.cpf
-      LIKE $1)
-      `,
-      [`%${cpf}%`]
+      `SELECT * FROM customers ${whereClause}`,
+      params
     );
 
     res.status(200).send(allCustomers);
-
+    
   } catch (error) {
     console.error(error);
     res.status(500).send("Bad Request");
   }
 }
 
-export { getCustomers };
+async function getCustomerByID(req, res) {}
+
+async function createCustomer(req, res) {}
+
+async function updateCustomers(req, res) {}
+
+export { getCustomers, getCustomerByID, createCustomer, updateCustomers };
--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
+import {
+  createCustomer,
+  getCustomerByID,
+  getCustomers,
+  updateCustomers,
+} from "../controllers/customersController.js";
 
 const customersRoutes = Router();
 
-customersRoutes.get("/customers");
-customersRoutes.post("/customers");
+customersRoutes.get("/customers", getCustomers);
+customersRoutes.get("/customers", getCustomerByID);
+customersRoutes.post("/customers", createCustomer);
+customersRoutes.put("/customers", updateCustomers);
 
 export { customersRoutes };
