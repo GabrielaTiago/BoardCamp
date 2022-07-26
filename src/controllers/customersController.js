9cfdc575d5735c878c35f5fe9401b3a8a32a4788
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-07-26 13:57:50 -0300

    Feat: Created the newCustomer controller

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -12,7 +12,7 @@ async function getCustomers(req, res) {
       whereClause += `WHERE cpf ILIKE $${params.length}`;
     }
 
-    const { rows: allCustomers, rowCount} = await connection.query(
+    const { rows: allCustomers, rowCount } = await connection.query(
       `SELECT * FROM customers ${whereClause}`,
       params
     );
@@ -36,12 +36,30 @@ async function getCustomerByID(req, res) {
 
   if (rowCount === 0) return res.sendStatus(404);
 
-
   res.status(200).send(customers);
 }
 
 async function newCustomer(req, res) {
+  const { name, phone, cpf, birthday } = req.body;
 
+  try {
+    const { rowCount } = await connection.query(
+      `SELECT * FROM customers WHERE name = $1`,
+      [name]
+    );
+
+    if (rowCount === 1) return res.sendStatus(409);
+
+    await connection.query(
+      `INSERT INTO customers (name, phone, cpf, birthday)
+        VALUES ($1, $2, $3, $4)`,
+      [name, phone, cpf, birthday]
+    );
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Bad Request");
+  }
 }
 
 async function updateCustomers(req, res) {}
--- src/schemas/newCustomerSchema.js
+++ src/schemas/newCustomerSchema.js
@@ -2,9 +2,9 @@ import joi from "joi";
 
 const newCustomerSchema = joi.object({
   name: joi.string().required(),
-  phone: joi.number().min(10).max(11).required(),
-  cpf: joi.number().min(11).max(11).required(),
-  birthday: joi.date().greater('01-01-1900').less('now').required()
+  phone: joi.string().min(10).max(11).required(),
+  cpf: joi.string().min(11).max(11).required(),
+  birthday: joi.date().iso().max("now").min("1900-01-01").required()
 });
 
 export { newCustomerSchema };
