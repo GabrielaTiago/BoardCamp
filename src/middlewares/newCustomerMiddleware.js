35f095d40efcae84de751cb8f83999a0ee2de7b2
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-07-26 12:28:11 -0300

    Chore: Created the customers middleware

---
+++ src/middlewares/newCustomerMiddleware.js
@@ -0,0 +1,14 @@
+import { newCustomerSchema } from "../schemas/newCustomer.js";
+
+async function newCustomerMiddleware(req, res, next) {
+
+  const validation = newCustomerSchema.validate(req.body, { abortEarly: true });
+
+  if (validation.error) {
+    return res.status(400).send(validation.error.details);
+  }
+
+  next();
+}
+
+export { newCustomerMiddleware };
--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -5,12 +5,13 @@ import {
   getCustomers,
   updateCustomers,
 } from "../controllers/customersController.js";
+import { newCustomerMiddleware } from "../middlewares/newCustomerMiddleware.js";
 
 const customersRoutes = Router();
 
 customersRoutes.get("/customers", getCustomers);
 customersRoutes.get("/customers/:id", getCustomerByID);
-customersRoutes.post("/customers", newCustomer);
+customersRoutes.post("/customers", newCustomerMiddleware, newCustomer);
 customersRoutes.put("/customers", updateCustomers);
 
 export { customersRoutes };
