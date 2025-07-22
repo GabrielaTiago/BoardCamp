84bf1a52260eb5832b2afa814758336ccf7a1674
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-07-22 11:29:13 -0300

    feat: Add schemas for category, customer, and game

---
+++ src/schemas/schemas.js
@@ -0,0 +1,11 @@
+import newCategorySchema from "./newCategorySchema.js";
+import newCustomerSchema from "./newCustomerSchema.js";
+import newGameSchema from "./newGameSchema.js";
+
+const schemas = {
+    "category": newCategorySchema,
+    "customer": newCustomerSchema,
+    "game": newGameSchema,
+}
+
+export default schemas;
