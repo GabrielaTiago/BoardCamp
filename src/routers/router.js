b1be05bb9dae152e5f3b35a1e8c67f53f0d13650
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-07-25 13:34:06 -0300

    docs: Add Swagger documentation

--- src/routers/router.js
+++ src/routers/router.js
@@ -3,9 +3,12 @@ import categoriesRouter from './categoriesRouter.js';
 import customersRouter from './customersRouter.js';
 import gamesRouter from './gamesRouter.js';
 import rentalsRouter from './rentalsRouter.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../../swagger.json' assert { type: 'json' };
 
 const router = Router();
 
+router.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use(categoriesRouter);
 router.use(customersRouter);
 router.use(gamesRouter);
