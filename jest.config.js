35ba94227f073790c54276f6c67382e17dc10279
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-07-27 21:27:32 -0300

    chore: Update Jest configuration and add setup file for mocking

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 export default {
+	silent: true,
 	collectCoverage: true,
 	coverageDirectory: 'coverage',
-	collectCoverageFrom: ['src/**/*.js'],
+	collectCoverageFrom: ['src/**/*.js', '!src/server.js', '!src/databases/**', '!src/schemas/**', '!src/tests/**', '!src/errors/**'],
+	setupFilesAfterEnv: ['./jest.setup.js'],
 };
